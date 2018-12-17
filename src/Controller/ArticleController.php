37883d7335fc6633f835ca07cf1a0f474e2c1a92
mathoutitou <fougeron.mathilde@gmail.com>
2018-12-17 17:25:31 +0100

    creation d'articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
@@ -12,24 +15,38 @@ class ArticleController extends AbstractController
     /**
      * @Route("/article", name="article")
      */
-    public function index()
+    public function index(ArticleRepository $repository)
     {
         return $this->render('article/index.html.twig', [
             'controller_name' => 'ArticleController',
+            'articles' => $repository->findAll(),
         ]);
     }
 
     /**
      * @Route("/article/new", name="article_new")
+     * @param EntityManagerInterface $manager
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function create()
+    public function create(EntityManagerInterface $manager, Request $request)
     {
         $form = $this->createForm(ArticleType::class, new Article());
 
-        dump($form);
+        $form->handleRequest($request);
 
-        return $this->render('article/index.html.twig', [
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('article');
+        }
+
+        return $this->render('article/create.html.twig', [
             'controller_name' => 'ArticleController',
+            'create_form' => $form->createView(),
         ]);
     }
 }
